823f36df3cf2daf0786c7850d95a1a1b512398e6
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2020-08-02 19:00:53 -0400

    fixed mongoURI in keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     MongoURI:
-mongoose.connect('mongodb+srv://nwcultivation:RootRoot!1>@cluster0.c850c.mongodb.net/nwc?retryWrites=true&w=majority');
+'mongodb+srv://nwcultivation:RootRoot!1>@cluster0.c850c.mongodb.net/nwc?retryWrites=true&w=majority'
 };
