211a5e82330a227fcd84465f73b4f44e02073026
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2020-08-03 12:10:51 -0400

    added timestamp and completed option in todo model

--- ToDoModel.js
+++ ToDoModel.js
@@ -5,6 +5,14 @@ const Schema = mongoose.Schema;
 const ToDoSchema = new Schema({
     todo: {
         type: String
+    },
+    todo_created: {
+        type: Date,
+        default: Date.now
+    },
+    todo_completed: {
+        type: Boolean,
+        default: false
     }
 });
 
